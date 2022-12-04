f32c8d4a616241756a2f2e1e6d892b32078f216f
Michael Morisy <m@morisy.com>
2022-12-04 18:23:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,8 @@
     GPT-3 Implementation as a DocumentCloud Add-on
 """
 
-import csv
-import re
-import time
-
+import csv, re, time, os, openai
 from documentcloud.addon import AddOn
-
-
-import os
-import openai
 
 openai.api_key = os.environ["TOKEN"]
 
@@ -28,66 +21,54 @@ class GPTPlay(AddOn):
             for document in search_results:
                 documents.append(str(document.id))
         
-        with open("compared_docs.csv", "w+") as file_:
-            writer = csv.writer(file_)
-            writer.writerow(
-                ["document_title", "url", "text", "output"]
-            )
-            user_input = self.data.get("prompt").translate(str.maketrans({"-":  r"\-",
-                                          "]":  r"\]",
-                                          "\\": r"\\",
-                                          "^":  r"\^",
-                                          "$":  r"\$",
-                                          "*":  r"\*",
-                                          ".":  r"\."})) 
+        if self.get_documents(): # Check to make sure there are documents:
+            with open("compared_docs.csv", "w+") as file_:
+                writer = csv.writer(file_)
+                writer.writerow(
+                    ["document_title", "url", "text", "output"]
+                )
+                user_input = self.data.get("prompt").translate(str.maketrans({"-":  r"\-",
+                                              "]":  r"\]",
+                                              "\\": r"\\",
+                                              "^":  r"\^",
+                                              "$":  r"\$",
+                                              "*":  r"\*",
+                                              ".":  r"\."})) 
 
-            self.set_message(f"Working on analyzing {str(len(self.documents))} documents.")
-            
-#            try:
-#                value = self.data.get("value")
-#                self.set_message("Saving the results as a value on the documents as well as generating CSV.")
-#            except:
-#                    self.set_message("Preparing to generate a CSV.")
-            
-            for doc_id in documents:
-                print("Beginning document iteration.")
-                try:
-                    document = self.client.documents.get(doc_id)
-#                    full_text = document.get_page_text(1) # Just starting with page one for now due to API limits.
-                    full_text = document.get_page_text(1).translate(str.maketrans({"-":  r"\-",
-                                          "]":  r"\]",
-                                          "\\": r"\\",
-                                          "^":  r"\^",
-                                          "$":  r"\$",
-                                          "*":  r"\*",
-                                          ".":  r"\."})) 
-                    submission="%s\n=========\n%s=========="%(user_input, full_text)
-                    print(submission)
-                    response = openai.Completion.create(
-                        model="text-davinci-002",
-                        prompt=submission,
-                        temperature=0.7,
-                        max_tokens=1000,
-                        top_p=1,
-                        frequency_penalty=0,
-                        presence_penalty=0
-                        )
-                    print("trying to print OpenAI raw response")
-                    print(response.choices[0].text)
-                    results = response.choices[0].text
-                    print(results)
+                self.set_message(f"Working on analyzing {str(len(self.documents))} documents.")
 
-#                    try:
-#                        response = results
-#                        response = results.split("Result: ")[-1]
-#                    except:
-#                        print("Extracting results failed.")
-                        
-                    writer.writerow([document.title, document.canonical_url, full_text, results])
-                        
-                except:
-                    print("Error, moving on to the next item.")
-            self.upload_file(file_)
+                for document in self.get_documents():
+                    print("Beginning document iteration.")
+                    try:
+                        # Just starting with page one for now due to API limits.
+                        full_text = document.get_page_text(1).translate(str.maketrans({"-":  r"\-", 
+                                              "]":  r"\]",
+                                              "\\": r"\\",
+                                              "^":  r"\^",
+                                              "$":  r"\$",
+                                              "*":  r"\*",
+                                              ".":  r"\."})) 
+                        submission="%s\n=========\n%s=========="%(user_input, full_text)
+                        response = openai.Completion.create(
+                            model="text-davinci-002",
+                            prompt=submission,
+                            temperature=0.7,
+                            max_tokens=1000,
+                            top_p=1,
+                            frequency_penalty=0,
+                            presence_penalty=0
+                            )
+                        results = response.choices[0].text
+                        print(results)
+                        writer.writerow([document.title, document.canonical_url, "full text goes here", "the answer"])
+                        writer.writerow([document.title, document.canonical_url, full_text, results])
+
+                    except:
+                        print("Error, moving on to the next item.")
+                self.upload_file(file_)
+            else:
+                self.set_message("It looks like no documents were selected. Search for some or select them and run again.")
+
 
 
 if __name__ == "__main__":
