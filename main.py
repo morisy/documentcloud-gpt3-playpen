8db6d3f191c391a2969693a0556bac26f65b2c73
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2023-12-04 14:32:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ class GPTPlay(AddOn):
                     message=[
                         {"role": "user", "content": submission}
                     ]
-                    response = client.chat.completions.create(messages=message, model=gpt_model, temperature=0.7, max_tokens=1000, top_p=1, frequency_penalty=0, presence_penalty=0)
+                    response = client.chat.completions.create(messages=message, model=gpt_model, temperature=0.2, max_tokens=1000, top_p=1, frequency_penalty=0, presence_penalty=0)
                     result = response.choices[0].message.content
                 
                     writer.writerow([document.title, document.canonical_url, result])
